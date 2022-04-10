d6e7839e967c76bd6efe435e2509e8f458ad0aa7
pabloempregoscom <pablo.rb@empregos.com.br>
2022-04-10 01:32:09 -0300

    🚀 feat: card has front/back and can be flipped 🚀

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Memory Game</h1>
+    <section class="game-board">
+      <AtomCard
+        v-for="(card, index)in cardList"
+        :key="`card-${index}`"
+        :value="card.value"
+        :visible="card.visible"
+        :position="card.position"
+        @select-card="flipCard"
+      />
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import AtomCard from "./components/atom/AtomCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AtomCard,
+  },
+  setup() {
+    const cardList = ref([]);
+
+    for (let i = 0; i < 16; i++) {
+      cardList.value.push({
+        value: i,
+        visible: false,
+        position: i,
+      });
+    }
+
+    const flipCard = (payload) => {
+      cardList.value[payload.position].visible = true;
+    };
+
+    return {
+      cardList,
+      flipCard,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  justify-content: center;
 }
 </style>
---
+++ src/components/AtomCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card">
+    <div v-if="visible" class="card-face is-front">
+      {{ value }}
+    </div>
+    <div class="card-face is-back">Back</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    position: {
+      type: Number,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, context) {
+    const selectCard = () => {
+      context.emit("select-card", {
+        position: props.position,
+      });
+    };
+    return {
+      selectCard,
+    };
+  },
+
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 5px solid #ccc;
+  position: relative;
+}
+
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+.card-face.is-front {
+  background-color: red;
+}
+</style>
